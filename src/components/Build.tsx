521d104a15df7a8489dd0734655e0b9471d4d8f0
XORS-eng <xorssoftware@gmail.com>
2024-06-28 19:53:50 -0700

    correctly centering text

--- src/components/Build.tsx
+++ src/components/Build.tsx
@@ -7,11 +7,11 @@ export default function Build() {
     <>
       <section className="hero bg-primary text-white h-full p-8">
         <div className="text-6xl font-bold mb-8">Build</div>
-        <div className="grid grid-cols-1 sm:grid-cols-2 lg:grid-cols-4 gap-4 flex align-center justify-center text-center">
-          <div className="border rounded-md p-10 border-4">
+        <div className="grid grid-cols-1 sm:grid-cols-2 lg:grid-cols-4 gap-4 text-center">
+          <div className="border rounded-md p-10 border-4 flex items-center justify-center">
             Build your first solution
           </div>
-          <div className="border rounded-md p-10 border-4">
+          <div className="border rounded-md p-10 border-4 flex items-center justify-center">
             Filecoin Docs Builder Cookbooks
           </div>
           <div className="border rounded-md p-10 border-4">FF grants</div>
--- src/components/Meet.tsx
+++ src/components/Meet.tsx
@@ -7,11 +7,11 @@ export default function Meet() {
     <>
       <section className="hero bg-primary text-white h-full p-8">
         <div className="text-6xl font-bold mb-8">Meet</div>
-        <div className="grid grid-cols-1 sm:grid-cols-2 lg:grid-cols-4 gap-4 flex align-center justify-center text-center ">
-          <div className="border rounded-md p-10 border-4">
+        <div className="grid grid-cols-1 sm:grid-cols-2 lg:grid-cols-4 gap-4 text-center ">
+          <div className="border rounded-md p-10 border-4 flex items-center justify-center">
             Filecoin Discord builder enablement channel
           </div>
-          <div className="border rounded-md p-10 border-4">
+          <div className="border rounded-md p-10 border-4 flex items-center justify-center">
             Filecoin Technical Forum
           </div>
         </div>
