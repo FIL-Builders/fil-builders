6cdd89eaafde79b6a993ce6f82fbaf14f5d3e717
XORS-eng <xorssoftware@gmail.com>
2024-06-28 19:46:45 -0700

    centering text

--- src/components/Build.tsx
+++ src/components/Build.tsx
@@ -7,7 +7,7 @@ export default function Build() {
     <>
       <section className="hero bg-primary text-white h-full p-8">
         <div className="text-6xl font-bold mb-8">Build</div>
-        <div className="grid grid-cols-1 sm:grid-cols-2 lg:grid-cols-4 gap-4 text-center">
+        <div className="grid grid-cols-1 sm:grid-cols-2 lg:grid-cols-4 gap-4 flex align-center justify-center text-center">
           <div className="border rounded-md p-10 border-4">
             Build your first solution
           </div>
--- src/components/Meet.tsx
+++ src/components/Meet.tsx
@@ -7,7 +7,7 @@ export default function Meet() {
     <>
       <section className="hero bg-primary text-white h-full p-8">
         <div className="text-6xl font-bold mb-8">Meet</div>
-        <div className="grid grid-cols-1 sm:grid-cols-2 lg:grid-cols-4 gap-4 text-center">
+        <div className="grid grid-cols-1 sm:grid-cols-2 lg:grid-cols-4 gap-4 flex align-center justify-center text-center ">
           <div className="border rounded-md p-10 border-4">
             Filecoin Discord builder enablement channel
           </div>
