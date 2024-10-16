d40cbdfbfc2a8573dbe42260b67342286231396e
xBalbinus <xiangan9@gmail.com>
2024-10-16 10:37:29 -0700

    push og

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -2,8 +2,6 @@ import "./globals.css";
 
 import { Inter } from "next/font/google";
 
-import Head from "next/head";
-
 const inter = Inter({ subsets: ["latin"] });
 
 const title = "FIL-Builders";
@@ -13,16 +11,16 @@ const url = "https://fil.builders/";
 export default function RootLayout({ children }) {
   return (
     <html lang="en">
-      <Head>
+      <head>
         <title>FIL Builders</title>
         <meta name="description" content="Cross the Filecoin chasm with us." />
         <link rel="icon" href="/favicon.ico" />
         <meta property="og:title" content={title} />
         <meta property="og:description" content={description} />
-        <meta property="og:url" content={url} />
         <meta property="og:image" content="/og.jpg" />
         <meta property="og:type" content="website" />
-      </Head>
+        <meta property="og:url" content={url} />
+      </head>
       <body className={inter.className}>{children}</body>
     </html>
   );
